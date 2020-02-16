37a7de631ad6818b392422e26ad3d1c0069b5bee
Humberto <chube_24@hotmail.com>
2020-02-16 12:25:19 -0700

    inital files

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <App  />,
+  rootElement
+);
