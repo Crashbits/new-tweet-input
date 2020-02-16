2fa7bba1ac98c818642d5479b9f31639c28aaa06
Humberto <chube_24@hotmail.com>
2020-02-16 13:11:22 -0700

    add basic logic to component

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,30 @@ import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
+function App() {
+  const [tweet, setTweet] = useState("")
+  useEffect(() => {
+    document.title = `${240 - tweet.length} character left.` 
+  }, [tweet])
+  return (
+    <div>
+      <textarea 
+        type="text"  
+        value={tweet} 
+        placeholder="add tweet" 
+        cols="30" 
+        rows="10" 
+        onChange={(e) => setTweet(e.target.value)}>
+      </textarea>
+      <button 
+        disabled={tweet.length === 0 || tweet.length > 240}
+        onClick={()=> console.log(tweet)}>
+        Tweet
+      </button>
+    </div>
 
+  )
+}
 
 
 const rootElement = document.getElementById("root");
